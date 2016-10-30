37de800baf4ce415329a4a3a83c7b6df016daeac
Sean Gorman <seanedwardgorman@gmail.com>
2016-10-30 16:13:42 -0700

    include Pizza.prototype small, medium and large. When size value is checked and submitted it is pushed to pizza.size.

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,36 @@ function Pizza(size, dough, topping, sauce, cost) {
   this.sauce = sauce;
 }
 
-Pizza.prototype.cost = function() {
-  
+Pizza.prototype.small = function() {
+  this.size = "small";
 }
 
+Pizza.prototype.medium = function() {
+   this.size = "medium";
+}
 
-var pizza = new Pizza();
+Pizza.prototype.large = function() {
+   this.size = "large";
+}
+
+var pizza = new Pizza("","",[],"",0);
+
+
+
+$(document).ready(function() {
+  $("form#formSize").submit(function() {
+    var size = $("input:radio[name=size]:checked").val();
+    if (size === "small") {
+      pizza.small();
+      alert(pizza.size)
+    }else if (size === "medium") {
+      pizza.medium();
+      alert(pizza.size)
+    }else if (size === "large") {
+      pizza.large();
+      alert(pizza.size);
+    } else {
+      alert("please enter a size.")
+    }
+  });
+});
