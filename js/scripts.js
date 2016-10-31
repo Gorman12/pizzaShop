9c09798d0d884bf84ce30b75c3d2c385da742dcc
Sean Gorman <seanedwardgorman@gmail.com>
2016-10-30 19:43:40 -0700

    Working functions dough, toppings and sauce. #formCustomize will add dough, topping and sauce to pizza apon submit.

--- js/scripts.js
+++ js/scripts.js
@@ -5,36 +5,47 @@ function Pizza(size, dough, topping, sauce, cost) {
   this.sauce = sauce;
 }
 
-Pizza.prototype.small = function() {
-  this.size = "small";
+
+Pizza.prototype.functionSize = function(input) {
+  var size = input;
+   this.size = size;
 }
 
-Pizza.prototype.medium = function() {
-   this.size = "medium";
+Pizza.prototype.functionDough = function(input) {
+  var dough = input;
+  this.dough = dough;
 }
 
-Pizza.prototype.large = function() {
-   this.size = "large";
+Pizza.prototype.functiontopping = function(input) {
+  var topping = input;
+  this.topping += topping;
 }
 
-var pizza = new Pizza("","",[],"",0);
+Pizza.prototype.functionSauce = function(input) {
+  var sauce = input;
+  this.sauce = sauce;
+}
+
+var pizza = new Pizza(0,0,[],0,0);
 
 
 
 $(document).ready(function() {
-  $("form#formSize").submit(function() {
-    var size = $("input:radio[name=size]:checked").val();
-    if (size === "small") {
-      pizza.small();
-      alert(pizza.size)
-    }else if (size === "medium") {
-      pizza.medium();
-      alert(pizza.size)
-    }else if (size === "large") {
-      pizza.large();
-      alert(pizza.size);
-    } else {
-      alert("please enter a size.")
-    }
+  $("form#formSize").submit(function(event) {
+    event.preventDefault();
+    var input = $("input:radio[name=size]:checked").val();
+    $(pizza.functionSize(input));
+  });
+  $("form#formCustomize").submit(function(event) {
+    event.preventDefault();
+    var dough = $("input:radio[name=dough]:checked").val();
+    var toppingArray = [];
+    $("input:checkbox[name=topping]:checked").each(function() {
+      toppingArray.push($(this).val());
+    });
+    var sauce = $("input:radio[name=sauce]:checked").val();
+    $(pizza.functionDough(dough));
+    $(pizza.functiontopping(toppingArray));;
+    $(pizza.functionSauce(sauce));
   });
 });
